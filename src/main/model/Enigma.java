f3be3fa94f658391f407288d1f07419084279f75
ce13ry <133625457+ce13ry@users.noreply.github.com>
2025-12-03 16:28:29 -0800

    spelling fix

--- src/main/model/Enigma.java
+++ src/main/model/Enigma.java
@@ -5,42 +5,42 @@ import java.util.*;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
-// represents an enigma machine with a collection of rotars
+// represents an enigma machine with a collection of rotors
 public class Enigma {
 
-    private ArrayList<Rotar> rotars;
+    private ArrayList<rotor> rotors;
 
     private String[] reflector = "SWJEDVZYQCXPOTMLIUANRFBKHGswjedvz qcxpotmliuanrfbkygh9876543210,.?!)(*#".split("");
     static final String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz 0123456789.,!?()#*";
 
-    // Effect: Creates a machine with no rotars
+    // Effect: Creates a machine with no rotors
     public Enigma() {
-        rotars = new ArrayList<>();
+        rotors = new ArrayList<>();
     }
 
     public void addSetting(int i, int initialPosition) {
-        EventLog.getInstance().logEvent(new Event("Added rotar: " + i + " with initial position: " + initialPosition));
-        rotars.add(new Rotar(i, initialPosition));
+        EventLog.getInstance().logEvent(new Event("Added rotor: " + i + " with initial position: " + initialPosition));
+        rotors.add(new rotor(i, initialPosition));
     }
 
-    // Effect: Removes the rotar at the given position
+    // Effect: Removes the rotor at the given position
     // Modifies: this
     // Requires: i is a valid position
     public void remove(int i) {
-        EventLog.getInstance().logEvent(new Event("Removed rotar: " + i));
-        rotars.remove(rotars.get(i - 1));
+        EventLog.getInstance().logEvent(new Event("Removed rotor: " + i));
+        rotors.remove(rotors.get(i - 1));
     }
 
     // Effect: Encrypts/Decrypts the input string
     // Modifies: this
-    // Modifies: rotars
+    // Modifies: rotors
     // Requires: Characters in the input string are in the alphabet
     @SuppressWarnings("methodlength") // 26 lines instead of 25
     public String cipher(String in) {
 
-        // reset all rotars
-        for (Rotar r : rotars) {
-            Rotar.reset(r);
+        // reset all rotors
+        for (rotor r : rotors) {
+            rotor.reset(r);
         }
         
         String[] input = in.split("");
@@ -56,7 +56,7 @@ public class Enigma {
             int character = defaultPosition(c);
 
             // forward direction
-            for (Rotar r : rotars) {
+            for (rotor r : rotors) {
                 input[pos] = r.getLetter(character);
                 character = defaultPosition(input[pos]);
             }
@@ -66,8 +66,8 @@ public class Enigma {
             character = defaultPosition(input[pos]);
 
             // backward direction
-            for (int i = rotars.size() - 1; i >= 0; i--) {
-                input[pos] = getDefaultLetter(getLetterPosition(input[pos], rotars.get(i)));
+            for (int i = rotors.size() - 1; i >= 0; i--) {
+                input[pos] = getDefaultLetter(getLetterPosition(input[pos], rotors.get(i)));
                 character = defaultPosition(input[pos]);
             }
 
@@ -79,23 +79,23 @@ public class Enigma {
         return toString(input);
     }
 
-    // Effect: Rotates nth rotar a whole loop before rotating n+1th rotar once
-    // Modifies rotars
+    // Effect: Rotates nth rotor a whole loop before rotating n+1th rotor once
+    // Modifies rotors
     public void update(int n) {
         
-        getRotars().get(n).rotate();
+        getrotors().get(n).rotate();
 
-        if (getRotars().get(n).getRotatePosition() == 0) {
-            if (n < getRotars().size() - 1) {
+        if (getrotors().get(n).getRotatePosition() == 0) {
+            if (n < getrotors().size() - 1) {
                 update(n + 1);
             }
         }
     }
 
-    // Effect: Returns the position of the given letter in the given rotar
-    public int getLetterPosition(String letter, Rotar rotar) {
-        for (int i = 0; i < Rotar.NUMOFCHARS; i++) {
-            if (rotar.getLetter(i).equals(letter)) {
+    // Effect: Returns the position of the given letter in the given rotor
+    public int getLetterPosition(String letter, rotor rotor) {
+        for (int i = 0; i < rotor.NUMOFCHARS; i++) {
+            if (rotor.getLetter(i).equals(letter)) {
                 return i;
             }
         }
@@ -112,11 +112,11 @@ public class Enigma {
         return ALPHABET.substring(i, i + 1);
     }
 
-    // Effect: Clears all rotars
+    // Effect: Clears all rotors
     // Modifies: this
     public void clear() {
-        EventLog.getInstance().logEvent(new Event("Cleared all rotars"));
-        rotars.clear();
+        EventLog.getInstance().logEvent(new Event("Cleared all rotors"));
+        rotors.clear();
     }
 
     // Effect: Array -> String
@@ -128,8 +128,8 @@ public class Enigma {
         return output;
     }
 
-    public ArrayList<Rotar> getRotars() {
-        return rotars;
+    public ArrayList<rotor> getrotors() {
+        return rotors;
     }
 
     // Effect: Returns true if the input contains invalid characters
@@ -144,15 +144,15 @@ public class Enigma {
 
     public JSONObject toJson() {
         JSONObject json = new JSONObject();
-        json.put("rotars", rotarsToJson());
+        json.put("rotors", rotorsToJson());
         return json;
     }
 
     // EFFECTS: returns things in this workroom as a JSON array
-    private JSONArray rotarsToJson() {
+    private JSONArray rotorsToJson() {
         JSONArray jsonArray = new JSONArray();
 
-        for (Rotar r : rotars) {
+        for (rotor r : rotors) {
             jsonArray.put(r.toJson());
         }
 
